dfea5db52eceabed817ef0866da45f95b5274b57
huiwy <kiriyamashinnji@gmail.com>
2021-08-26 00:55:22 +0800

    finished player, not validated

--- js/player.js
+++ js/player.js
@@ -1,3 +1,73 @@
 class Player {
-  
+  constructor() {
+    this.loop = false;
+    this.random = false;
+    this.overlap = false;
+    this.playList = [];
+    this.soundList = [];
+  }
+
+  play(url) {
+    if (!self.overlap) {
+      this.pause();
+      this.playList = [];
+    }
+    let audio = new Audio(url);
+    audio.player = this;
+    audio.onended = audioEnd;
+    this.playList.push(audio);
+    audio.loop = self.loop;
+    audio.play();
+  }
+
+  pause() {
+    this.playList.forEach((audio) => {
+      audio.pause();
+    });
+  }
+
+  continue() {
+    this.playList.forEach((audio) => {
+      audio.play();
+    });
+  }
+
+  setMode(mode, overlap) {
+    if (mode == "loop") {
+      this.playList.forEach((sound) => {
+        sound.loop = true;
+      });
+    } else if (mode == "no-loop") {
+      this.playList.forEach((sound) => {
+        sound.loop = false;
+      });
+    }
+
+    this.overlap = overlap;
+    this.random = mode == "random";
+  }
+
+  remove(audio) {
+    let newPlayList = [];
+    this.playList.forEach((sound) => {
+      if (sound == audio) sound.pause();
+      else newPlayList.push(sound);
+    });
+    this.playList = newPlayList;
+  }
+
+  playRandom() {
+    let idx = randint(0, this.soundList.length - 1);
+    this.play(this.soundList[idx]);
+  }
+}
+
+function audioEnd() {
+  if (this.player.playList.length == 1 && this.player.random)
+    this.player.playRandom();
+  if (this.ended) this.player.remove(this);
+}
+
+function randint(l, h) {
+  return Math.floor(Math.random() * (h - l + 1)) + l;
 }
--- js/widgets.js
+++ js/widgets.js
@@ -1,6 +1,17 @@
 let leaves = [$("#leaf-1"), $("#leaf-2"), $("#leaf-3"), $("#leaf-4")];
 let expand = true;
 
+let l2Text = ["暂停播放", "继续播放"];
+let l2Img = ["assets/pause.svg", "assets/continue.svg"];
+let l3Text = ["禁止重叠", "重叠播放"];
+let l3Img = ["assets/non-overlapping.svg", "assets/overlapping.svg"];
+let l4Text = ["禁止循环", "洗脑循环", "随机循环"];
+let l4Img = ["assets/no-loop.svg", "assets/loop.svg", "assets/random.svg"];
+let l4Mode = ["no-loop", "loop", "random"];
+let l2Counter = 0;
+let l3Counter = 0;
+let l4Counter = 0;
+
 function foldLeaves() {
   if (expand) {
     $(".expanded-leaf").addClass("l-folded");
@@ -15,7 +26,33 @@ function foldLeaves() {
     $(".control-leaf").addClass("l-folded");
     expand = true;
   }
-
 }
 
-$(".control-leaf").click(() => foldLeaves())
+$(".control-leaf").click(() => foldLeaves());
+
+$(".l-2").click(() => {
+  l2Counter++;
+  $(".l-2.leaf-content").attr("src", l2Img[l2Counter % 2]);
+  $(".l-2.leaf-description").text(l2Text[l2Counter % 2]);
+
+  if (l2Counter % 2 == 1) player.pause();
+  else player.continue();
+});
+
+$(".l-3").click(() => {
+  l3Counter++;
+  $(".l-3.leaf-content").attr("src", l3Img[l3Counter % 2]);
+  $(".l-3.leaf-description").text(l3Text[l3Counter % 2]);
+  player.setMode(l4Mode[l4Counter % 3], l3Counter % 2 == 1);
+});
+
+$(".l-4").click(() => {
+  l4Counter++;
+  $(".l-4.leaf-content").attr("src", l4Img[l4Counter % 3]);
+  $(".l-4.leaf-description").text(l4Text[l4Counter % 3]);
+  player.setMode(l4Mode[l4Counter % 3], l3Counter % 2 == 1);
+});
+
+$(".l-5").click(() => {
+  player.playRandom();
+});
